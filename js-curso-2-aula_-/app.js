bb1d8f29c4e719a667b78123f7e635d8bbe599c1
Malone-Campelo <malonycampelo1@gmail.com>
2024-05-19 22:04:29 -0300

    Projeto de estudo, jogo do número secreto

---
+++ js-curso-2-aula_-/app.js
@@ -0,0 +1,50 @@
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+function exibirMensagemInicial() {
+exibirTextoNaTela('h1','Jogo Do Número Secreto.');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10.');
+}
+
+exibirMensagemInicial();
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Você Acertou!!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Voce descobriu o numero secreto com ${tentativas} ${palavraTentativa}.`
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'Número secreto é menor');
+        } else {
+            exibirTextoNaTela('p', 'Número secreto é maior');
+        }
+    }
+    tentativas++
+    limparCampo();
+}
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
+}
+
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+}
+
